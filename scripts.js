48f6a0ee0eb314b677550c08cb88c11b06d0fdf1
Josh Lee <joshswl@gmail.com>
2018-01-25 21:21:12 -0600

    logs minutes submitted

--- scripts.js
+++ scripts.js
@@ -47,3 +47,8 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+})
