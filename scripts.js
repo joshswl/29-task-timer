478df6c523b1e773a6d3116968a7c94553c53c36
Josh Lee <joshswl@gmail.com>
2018-01-25 20:23:47 -0600

    countdown does not go below 0

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
-    setInterval(() => {
+
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+
+        // display it
         console.log(secondsLeft);
     }, 1000);
 }
