f5328113f56f34e65bccadc31426ef14ba347cf7
Josh Lee <joshswl@gmail.com>
2018-01-25 20:37:01 -0600

    added '0' for remainderSeconds < 10

--- scripts.js
+++ scripts.js
@@ -22,6 +22,6 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds}`;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
     timerDisplay.textContent = display;
 }
